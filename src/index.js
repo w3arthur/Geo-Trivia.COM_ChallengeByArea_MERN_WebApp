8642e247deb03239c3a26292fc3263928ae07974
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-04-11 18:00:45 +0300

    first push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
+import '../src/components/i18next';
 import './index.css';
 import App from './App';
 
-ReactDOM.render( <React.StrictMode> <App /> </React.StrictMode>, document.getElementById('root') );
+ReactDOM.render(<Suspense fallback={(<div>Loading</div>)}> <React.StrictMode> <App /> </React.StrictMode></Suspense>, document.getElementById('root') );
 
