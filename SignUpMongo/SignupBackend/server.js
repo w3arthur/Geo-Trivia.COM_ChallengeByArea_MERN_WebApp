d7d1f8df5eb9b41deea5bd8e7fc25e6ce6e421a5
sleem3232 <sleem3232@gmail.com>
2022-04-15 04:07:07 +0300

    Salim14-04-2022

---
+++ SignUpMongo/SignupBackend/server.js
@@ -0,0 +1,13 @@
+const express= require('express')
+const app =express()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const routeurls=require('./routes/routes')
+const cors=require('cors')
+dotenv.config()
+
+mongoose.connect(process.env.DATABASE_ACCESS,()=>console.log("database connected!!"))
+app.use(express.json())
+app.use(cors())
+app.use('/app',routeurls)
+app.listen(4000,()=>console.log("the server is running"))
