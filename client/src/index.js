5ea3583a6bc9562c1929fecd22d13f5127c52408
w3arthur <w3arthur@gmail.com>
2022-10-13 22:28:30 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,22 @@
 import React, {  } from "react";
 import './api/i18next'
 import App from "./App";
-
-import ReactDOM from 'react-dom';
-//import { createRoot } from 'react-dom/client';
-
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-// root.render(<React.StrictMode>  <App /> </React.StrictMode>);
+//import ReactDOM from 'react-dom';  //react 18
+import * as ReactDOM from 'react-dom';
 
 
-ReactDOM.render(
+
+
+//react 17
+
+ ReactDOM.render(<React.StrictMode>  <App /> </React.StrictMode>, document.getElementById('root'));
+
+
+
+/*
+ReactDOM.render(//react 18
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+);*/
