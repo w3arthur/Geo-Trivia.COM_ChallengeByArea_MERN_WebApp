314c06ad3f25ace7c4b3d18f523011c2f70292ab
w3arthur <w3arthur@gmail.com>
2022-10-13 20:52:20 +0300

    update

--- client/src/Hooks/useGoTo404.js
+++
@@ -1,6 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-export default function useGoTo404(){
-    const navigate = useNavigate();
-    return function goTo(){ navigate( '/Error' , { replace: true });  }
-}
