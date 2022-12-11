232992b38befe8ea1ca3894cebf9e8e1d79c98ea
w3arthur <w3arthur@gmail.com>
2022-12-11 17:29:12 +0200

    deploy

--- server/menoraflix/server.js
+++ server/menoraflix/server.js
@@ -1,7 +1,7 @@
 const { ports } = require('./config');
 const serverPort = ports.SERVER_PORT;
 let developerPort = undefined;
-if (process.env.NODE_ENV !== 'production'){ developerPort = ports.SERVER_DEVELOPER_PORT; }
+if (process.env.NODE_ENV !== 'production') { developerPort = ports.SERVER_DEVELOPER_PORT; }
 
 const express = require("express");
 const app = express();
@@ -13,7 +13,7 @@ app.use(require("cors")());     //Access-Control-Allow-Credentials: true
 app.use(require("cookie-parser")());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(middlewares.globalErrorMainHandler);
 
 
@@ -22,8 +22,8 @@ app.use(middlewares.globalErrorMainHandler);
 const routers = require("./routers");
 
 const public_folder = 'public_folder';
-app.use("/menoraflix/", express.static(path.join(__dirname, public_folder)));  //global folder
-app.route("/menoraflix/").get(async (req, res) => res.status(200).sendFile(path.join(__dirname, public_folder, "index.html")) );
+//app.use("/menoraflix/", express.static(path.join(__dirname, public_folder)));  //global folder
+app.route("/menoraflix/").get(async (req, res) => res.status(200).sendFile(path.join(__dirname, public_folder, "index.html")));
 app.use("/menoraflix/api/login", routers.loginRouter); //verifyJWT set inside for not registration part!
 
 // //if (process.env.NODE_ENV === 'production') 
@@ -37,7 +37,7 @@ app.use("/menoraflix/api/movie", routers.movieRouter);
 app.use("/menoraflix/api/favorites", routers.favoritesRouter);
 
 
-app.route("*").all((req, res) => res.status(404) );
+app.route("*").all((req, res) => res.status(404));
 app.use(middlewares.errorMainHandler); //errorHandler
 const port = developerPort || serverPort || 3500;
 app.listen(port, () => console.log(`Listening on port ${port}, Express`));
